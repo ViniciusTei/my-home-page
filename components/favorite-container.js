ac3197832a77222e9a3d811ac72b88d71e475093
ViniciusTei <viniciustprates@gmail.com>
2024-01-14 11:34:59 -0300

    wip web component favorite container

---
+++ components/favorite-container.js
@@ -0,0 +1,66 @@
+const template = document.createElement("template")
+template.innerHTML = `
+  <a data-link href="" class="min-w-[70px] text-center">
+    <div class="border-1 border-black rounded shadow-md flex items-center justify-center p-4">
+      <i data-icon class="fa-brands fa-2xl"></i>
+    </div>
+    <p data-label></p>
+  </a>
+`
+
+class FavoriteContainer extends HTMLElement {
+  constructor() {
+    super()
+    const shadow = this.attachShadow({ mode: "open" })
+    shadow.append(template.content.cloneNode(true))
+
+    this.link = shadow.querySelector("[data-link]")
+    this.icon = shadow.querySelector("[data-icon]")
+    this.label = shadow.querySelector("[data-label]")
+  }
+
+  static get observedAttributes() {
+    return ["icon", "color", "label", "link"]
+  }
+ 
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(name, oldValue, newValue)
+    switch (name) {
+      case "icon":
+        this.updateIcon(newValue) 
+        break;
+      case "color":
+        this.updateColor(newValue) 
+        break;
+      case "link":
+        this.updateLink(newValue) 
+        break;
+      case "label":
+        this.updateLabel(newValue) 
+        break;
+      default:
+        console.log('No property with this name.')
+        break;
+    }
+  }
+
+  updateIcon(icon) {
+    this.icon.classList.add(icon)
+  }
+
+  updateColor(color) {
+    this.icon.style.color = color
+  }
+
+  updateLabel(label) {
+    this.label.innerText = label
+  }
+
+  updateLink(link) {
+    this.link.setAttribute("href", link)
+  }
+
+}
+
+customElements.define("favorite-container", FavoriteContainer)
+
